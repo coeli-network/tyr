2aa09b932f9c3d5913caf62402d22b2e60468542
jpfeiffer16 <joepfeiffer16@gmail.com>
2024-07-25 17:23:08 -0400

    move some code over

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,26 @@
 const std = @import("std");
 
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+pub fn main() !u8 {
+    const stdout = std.io.getStdOut().writer();
+    const stderr = std.io.getStdErr().writer();
+    const args = try std.process.argsAlloc(std.heap.page_allocator);
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    if (args.len != 2) {
+        try stderr.print("one arg expected <nock code>\n", .{});
+        return 1;
+    }
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    var path_buffer: [std.fs.MAX_PATH_BYTES]u8 = undefined;
+    const path = std.fs.realpath(args[1], &path_buffer) catch |err| switch(err) {
+        error.FileNotFound => {
+            try stderr.print("could not find nock file\n", .{});
+        },
+        else => {
+            try stderr.print("unexpected error: ${}\n", .{ err });
+        }
+    };
 
-    try bw.flush(); // don't forget to flush!
-}
+    try stdout.print("path: {s}", .{ path });
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    return 0;
 }
